04d92140ef65cc34cbd0cc76aec42873aa38081b
Noise290 <noisemonster29@gmail.com>
2026-01-28 14:57:27 +0100

    SEO update

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -36,18 +36,20 @@ const Landing = () => {
             }
         }
     }, [location, navigate]);
-
     return (
         <>
             <Helmet>
-                <title>Vitema - Aplikacja dla dietetyków i trenerów personalnych</title>
-                <meta name="description"
-                      content="Vitema to kompleksowe narzędzie do układania diet, zarządzania pacjentami i automatyzacji pracy dietetyka. Sprawdź nasze funkcje!"/>
-                <meta name="keywords"
-                      content="Vitema, program dla dietetyka, układanie diet, aplikacja dietetyczna, trener personalny"/>
+                <title>Vitema - Aplikacja dla Trenerów Personalnych i Dietetyków | Szybkie diety online</title>
 
-                <meta property="og:title" content="Vitema - Twój asystent żywienia"/>
-                <meta property="og:description" content="Przyspiesz swoją pracę z pacjentami dzięki Vitema."/>
+                <meta name="description"
+                      content="Vitema to narzędzie dla Trenerów i Dietetyków. Układaj jadłospisy w 5 minut, generuj automatyczną listę zakupów i prowadź podopiecznych w dedykowanej aplikacji mobilnej. Zastąp Excela nowoczesnym systemem."/>
+
+                <meta name="keywords"
+                      content="Vitema, aplikacja dla trenera personalnego, program do układania diet, współpraca online, aplikacja dietetyczna, dieta dla podopiecznego, lista zakupów app"/>
+
+                <meta property="og:title" content="Vitema - Twoja aplikacja do prowadzenia podopiecznych"/>
+                <meta property="og:description"
+                      content="Koniec z PDF-ami. Daj podopiecznym dietę i listę zakupów w aplikacji na telefon. Sprawdź Vitemę!"/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:url" content="https://vitema.pl/"/>
             </Helmet>
